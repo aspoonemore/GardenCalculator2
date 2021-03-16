5bb924de28545467749452e763e94ccaaa797ac6
Dale Spoonemore <aspoonemore@gmail.com>
2021-03-16 12:17:29 -0500

    week 3 changes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,42 +1,39 @@
 import { Component } from '@angular/core';
-
 export interface RaisedBed {
+  raisedBedName: string;
   raisedBedLengthFeet: number;
   raisedBedWidthFeet: number;
   raisedBedHeightFeet: number;
   soilVolume: number;
 }
-
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  raisedBedName: string;
   raisedBedLengthFeet: number = 8;
   raisedBedWidthFeet: number = 4;
   raisedBedHeightInches: number = 12;
   soilVolume: number;
   testGarden: RaisedBed[] = [];
   totalVolume: number = 0;
-
   constructor() {}
-
   addToGardenList() {
     let raisedBedHeightFeet = this.raisedBedHeightInches / 12;
     this.soilVolume = raisedBedHeightFeet * this.raisedBedLengthFeet * this.raisedBedWidthFeet;
     let newTestRaisedBed: RaisedBed = {
+      raisedBedName: this.raisedBedName,
       raisedBedHeightFeet: raisedBedHeightFeet,
       raisedBedLengthFeet: this.raisedBedLengthFeet,
       raisedBedWidthFeet: this.raisedBedWidthFeet,
-      soilVolume: this.soilVolume
+      soilVolume: this.soilVolume,
     }
-
     this.testGarden.push(newTestRaisedBed);
     this.updateTotalVolume(this.soilVolume);
     console.log(this.testGarden)
   }
-
   updateTotalVolume(newVolume: number) {
     this.totalVolume += newVolume;
   }
